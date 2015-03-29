1aa2440f9cda14034e891b7af0809caa7f0fac14
steve <countryman2k@hotmail.com>
2015-03-29 14:06:34 -0700

    file

--- createpdf.php
+++ createpdf.php
@@ -15,4 +15,6 @@ $pdf->Cell(200,10,"CELL TEST 200,10");
 $pdf->Cell(40,10,"STEVE");
 $pdf->Image('http://chart.googleapis.com/chart?cht=p3&chd=t:60,40&chs=250x100&chl=Hello|World',60,30,90,0,'PNG');
 $pdf->Output();
+
+echo "hello";
 ?>
